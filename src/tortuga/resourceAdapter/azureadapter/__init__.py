4e6505b9ba488c525c0c9d1a9931449d21a4ff53
Mike Frisch <mfrisch@univa.com>
2018-06-15 08:00:08 -0400

    Remove unnecessary namespace declaration

--- src/tortuga/resourceAdapter/azureadapter/__init__.py
+++ src/tortuga/resourceAdapter/azureadapter/__init__.py
@@ -12,6 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__import__('pkg_resources').declare_namespace(__name__)
-
 from .azureadapter import AzureAdapter
