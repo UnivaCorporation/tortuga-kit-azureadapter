815b5a9efac9aa20e722d9bcf44a4016711a9b92
Mike Frisch <mfrisch@univa.com>
2019-02-14 15:33:16 -0500

    Remove unused import

--- src/tortuga/resourceAdapter/azureadapter/azureadapter.py
+++ src/tortuga/resourceAdapter/azureadapter/azureadapter.py
@@ -44,7 +44,6 @@ from tortuga.db.models.softwareProfile import SoftwareProfile
 from tortuga.db.nodesDbHandler import NodesDbHandler
 from tortuga.exceptions.configurationError import ConfigurationError
 from tortuga.exceptions.nodeNotFound import NodeNotFound
-from tortuga.exceptions.operationFailed import OperationFailed
 from tortuga.exceptions.resourceNotFound import ResourceNotFound
 from tortuga.exceptions.tortugaException import TortugaException
 from tortuga.node import state
